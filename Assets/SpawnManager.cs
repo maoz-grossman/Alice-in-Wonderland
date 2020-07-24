07b49396c120b9c8bc05278f3bb2c1e87b5574b0
Maoz-Grossman <44970530+maoz-grossman@users.noreply.github.com>
2020-07-24 11:45:06 +0300

    Game first look
    Game

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _TilePrefab;
+    private Vector3 _spawnPos;
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spawnPos.z += 30;
+        StartCoroutine(SpawnTileRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTileRoutine(){
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(_TilePrefab, _spawnPos, _TilePrefab.transform.rotation);
+        _spawnPos.z+=6;
+        StartCoroutine(SpawnTileRoutine());
+    }
+}
---
+++ Assets/TileBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed=3f;
+    private CharacterController _controller;
+    private float _gravity = 9.807f;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Tile")
+        {
+            Debug.Log("Hey");
+            Destroy(other.gameObject, 1f);
+        }
+    }
+
+    private void Movement()
+    {
+        Vector3 direction = new Vector3(0, 0, 1);
+        Vector3 velocity = direction * _speed;
+
+        if (!_controller.isGrounded)
+        {
+            velocity.y -= _gravity;
+        }
+        
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
