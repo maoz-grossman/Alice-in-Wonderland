273f3ec75abfd3a592d310f04701d799e6334697
maoz-grossman <maozg2008@gmail.com>
2020-07-24 15:48:00 +0300

    PowerUp Handler :video_game:

---
+++ Assets/Scripts/Player_PowerUps_Handler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Update values acordding to the recived PowerUp
+*/
+public class Player_PowerUps_Handler : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tr = GetComponent<Transform>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_tr.localScale += new Vector3(.01f, .01f, .01f);
+    }
+}
