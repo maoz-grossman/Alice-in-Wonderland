312d48ca4926c44b2250c4d5cfb3ae16b5674d54
maoz-grossman <maozg2008@gmail.com>
2020-07-28 20:25:58 +0300

    :video_game:

--- Assets/Scripts/CameraPref.cs
+++ Assets/Scripts/CameraPref.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class CameraPref : MonoBehaviour
 {
+    public static float X_pos = 0;
+    public static float Y_pos = 0;
+    public static float Z_pos = 1;
+    public static bool follow_player = true;
     [SerializeField]
     private Transform LookAt;
     [SerializeField]
-    private Vector3 offset= new Vector3(0, 5.0f, -3f);
+    private Vector3 offset = new Vector3(0, 5.0f, -3f);
+
     // Start is called before the first frame update
-   // void Start()
-   // {
+    // void Start()
+    // {
     //    GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 10);
-   // }
+    // }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 desiredPostion = LookAt.position + offset;
-        desiredPostion.x = 0;
-        transform.position = LookAt.position + offset;
+        if (follow_player)
+        {
+            Vector3 desiredPostion = LookAt.position + offset;
+            desiredPostion += new Vector3(0, Y_pos, 0);
+            desiredPostion.z += Z_pos; 
+            desiredPostion.x = 1.76f+ X_pos;
+            transform.position = desiredPostion;
+        }
     }
 }
